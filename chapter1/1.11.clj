350b56b1000d7c71b6f721f1366645f633008bca
Curtis Badke <curtis@badkid.ca>
2013-05-08 06:40:28 -0600

    add iterative impl for 1.11

--- chapter1/1.11.clj
+++ chapter1/1.11.clj
@@ -7,10 +7,25 @@
       (* 2 (f-recursive (- n 2)))
       (* 3 (f-recursive (- n 3))))))
 
-(println "recursive " (f-recursive 12))
+(println "recursive f(1)"  (f-recursive 1))
+(println "recursive f(3)"  (f-recursive 3))
+(println "recursive f(8)"  (f-recursive 8))
+(println "recursive f(12)" (f-recursive 12))
 
 (defn f-iterative [n]
-  (defn f-iter [n] n)
-  (f-iter n))
+  (defn f-iter [a, b, c, curr]
+    (if (> curr n)
+      a
+      (f-iter 
+        (+ a (* b 2) (* c 3)) 
+        a 
+        b 
+        (+ curr 1))))
+  (if (< n 3)
+    n
+    (f-iter 2 1 0 3)))
 
-(println "iterative " (f-iterative 12))
+(println "recursive f(1)"  (f-iterative 1))
+(println "recursive f(3)"  (f-iterative 3))
+(println "recursive f(8)"  (f-iterative 8))
+(println "recursive f(12)" (f-iterative 12))
